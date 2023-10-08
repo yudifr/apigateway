0f8ae18ada6176109b8ed3abe647a4eec13f2b23
yudi <yudi@tsp.dev>
2023-10-08 21:04:55 +0700

    08/10 update

--- routers/tracerRouter.py
+++ routers/tracerRouter.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI, APIRouter, Request
+from fastapi import FastAPI, APIRouter, Request, HTTPException
 
 import requests
 
 import json
 router = APIRouter()
 PORT = '8004'
+INSTITUTION_PORT = '8001'
 ALUMNI_PORT = '8002'
 CONSUMER_PORT = '8003'
 URL = 'http://localhost:'
@@ -38,23 +39,34 @@ def getAlumniKuisionerByInstitution(institutionId: str, institutionType: str):
         'institution_type': institutionType
 
     }
-    response = requests.post(URL+ALUMNI_PORT+'/alumni/getalumnidata', data=json.dumps(alumniDataQueries), headers={
+    urlToGetIds = URL+ALUMNI_PORT+'/alumni/getalumnidata'
+    if institutionType == '4':
+        urlToGetIds = URL+INSTITUTION_PORT+'/institution/kuisioner/alumni'
+    print(urlToGetIds)
+    response = requests.post(urlToGetIds, data=json.dumps(alumniDataQueries), headers={
         'app-origins': 'yes',
         'Content-Type': 'application/json',
     })
+    print(response)
     responseData = response.json()
     alumniFromInstitutionData = responseData.get('data')
-
-    # todo get by in
-    alumniKuisionerPayload = {
-        'id_alumnis': alumniFromInstitutionData
-    }
-    responseTracer = requests.post(URL+PORT+'/kuisioner/get-multiple-alumni-kuisioner', data=json.dumps(alumniKuisionerPayload), headers={
-        'app-origins': 'yes',
-        'Content-Type': 'application/json',
-    })
-    responseTracerData = responseTracer.json()
-    return responseTracerData
+    print(alumniFromInstitutionData)
+    print(len(alumniFromInstitutionData))
+    if len(alumniFromInstitutionData) > 0:
+        alumniKuisionerPayload = {
+            'id_alumnis': alumniFromInstitutionData
+        }
+        responseTracer = requests.post(URL+PORT+'/kuisioner/get-multiple-alumni-kuisioner', data=json.dumps(alumniKuisionerPayload), headers={
+            'app-origins': 'yes',
+            'Content-Type': 'application/json',
+        })
+        responseTracerData = responseTracer.json()
+        return responseTracerData
+    else:
+        raise HTTPException(
+            status_code=400,
+            detail="No Kuisioner Available Yet",
+        )
 
 
 @router.get('/kuisioner/institution/consumer/{institutionId}/{institutionType}', name="Get consumer Kuisioner By Institution")
@@ -64,23 +76,48 @@ def getConsumerKuisionerByInstitution(institutionId: str, institutionType: str):
         'institution_type': institutionType
 
     }
-    response = requests.post(URL+ALUMNI_PORT+'/alumni/getworkerdata', data=json.dumps(consumerDataQueries), headers={
+    urlToGetIds = URL+ALUMNI_PORT+'/alumni/getworkerdata'
+    if institutionType == '4':
+        urlToGetIds = URL+INSTITUTION_PORT+'/institution/kuisioner/alumni'
+    print(urlToGetIds, 'ffffffffffffffff')
+    response = requests.post(urlToGetIds, data=json.dumps(consumerDataQueries), headers={
         'app-origins': 'yes',
         'Content-Type': 'application/json',
     })
     responseData = response.json()
     consumerFromInstitutionData = responseData.get('data')
-
-    # todo get by in
-    consumerKuisionerPayload = {
-        'id_consumer': consumerFromInstitutionData
-    }
-    responseTracer = requests.post(URL+PORT+'/kuisioner/get-multiple-consumer-kuisioner', data=json.dumps(consumerKuisionerPayload), headers={
-        'app-origins': 'yes',
-        'Content-Type': 'application/json',
-    })
-    responseTracerData = responseTracer.json()
-    return responseTracerData
+    print(consumerFromInstitutionData)
+    if len(consumerFromInstitutionData) > 0:
+        idsfff = {
+            'ids': consumerFromInstitutionData
+        }
+        consumer = requests.post(URL+ALUMNI_PORT+'/alumni/get-consumer-id', data=json.dumps(idsfff), headers={
+            'app-origins': 'yes',
+            'Content-Type': 'application/json',
+        })
+        consumerData = consumer.json()
+        print(consumerData)
+        consData = consumerData.get('data')
+        if len(consData) > 0:
+            consumerKuisionerPayload = {
+                'id_consumer': consData
+            }
+            responseTracer = requests.post(URL+PORT+'/kuisioner/get-multiple-consumer-kuisioner', data=json.dumps(consumerKuisionerPayload), headers={
+                'app-origins': 'yes',
+                'Content-Type': 'application/json',
+            })
+            responseTracerData = responseTracer.json()
+            return responseTracerData
+        else:
+            raise HTTPException(
+                status_code=400,
+                detail="No Kuisioner Available Yet f",
+            )
+    else:
+        raise HTTPException(
+            status_code=400,
+            detail="No Kuisioner Available Yet",
+        )
 
 
 @router.post('/kuisioner/alumni', name='Post tracer')
@@ -96,14 +133,14 @@ async def newAlumniKuisioner(request: Request):
         'kerja_pertama': form.get('kerja_pertama', ''),
         'sesuaikah': form.get('sesuaikah', 'false'),
         'syaratipk': form.get('syaratipk', 'false'),
-        'kuisioner1': form.get('kuisioner1'),
-        'kuisioner2': form.get('kuisioner2'),
-        'kuisioner3': form.get('kuisioner3'),
-        'kuisioner4': form.get('kuisioner4'),
-        'kuisioner5': form.get('kuisioner5'),
-        'kuisioner6': form.get('kuisioner6'),
-        'kuisioner7': form.get('kuisioner7'),
-        'kuisioner8': form.get('kuisioner8'),
+        'kuisioner1': form.get('kuisioner1', ''),
+        'kuisioner2': form.get('kuisioner2', ''),
+        'kuisioner3': form.get('kuisioner3', ''),
+        'kuisioner4': form.get('kuisioner4', ''),
+        'kuisioner5': form.get('kuisioner5', ''),
+        'kuisioner6': form.get('kuisioner6', ''),
+        'kuisioner7': form.get('kuisioner7', ''),
+        'kuisioner8': form.get('kuisioner8', ''),
     }
     response = requests.post(URL+PORT+'/kuisioner/alumni', data=json.dumps(formData), headers={
         'app-origins': "yes",
@@ -126,14 +163,14 @@ async def updateAlumniKuisioner(request: Request, id: str):
         'kerja_pertama': form.get('kerja_pertama', ''),
         'sesuaikah': form.get('sesuaikah', ''),
         'syaratipk': form.get('syaratipk', ''),
-        'kuisioner1': form.get('kuisioner1'),
-        'kuisioner2': form.get('kuisioner2'),
-        'kuisioner3': form.get('kuisioner3'),
-        'kuisioner4': form.get('kuisioner4'),
-        'kuisioner5': form.get('kuisioner5'),
-        'kuisioner6': form.get('kuisioner6'),
-        'kuisioner7': form.get('kuisioner7'),
-        'kuisioner8': form.get('kuisioner8'),
+        'kuisioner1': form.get('kuisioner1', ''),
+        'kuisioner2': form.get('kuisioner2', ''),
+        'kuisioner3': form.get('kuisioner3', ''),
+        'kuisioner4': form.get('kuisioner4', ''),
+        'kuisioner5': form.get('kuisioner5', ''),
+        'kuisioner6': form.get('kuisioner6', ''),
+        'kuisioner7': form.get('kuisioner7', ''),
+        'kuisioner8': form.get('kuisioner8', ''),
     }
     response = requests.put(URL+PORT+'/kuisioner/alumni/'+id, data=json.dumps(formData), headers={
         'app-origins': "yes",
@@ -148,13 +185,12 @@ async def updateAlumniKuisioner(request: Request, id: str):
 async def newConsumerKuisioner(request: Request):
     form = await request.json()
     formData = {
-        'id_consumer': form.get('id_consumer'),
-        'saran_prodi': form.get('saran_prodi'),
-        'saran_fasilitas': form.get('saran_fasilitas'),
-        'saran_dosen': form.get('saran_dosen'),
-        'saran_kurikulum': form.get('saran_kurikulum'),
-        'saran_administrasi': form.get('saran_administrasi'),
-        'kekurangan': form.get('kekurangan'),
+        'id_consumer': form.get('id_consumer', ''),
+        'saran_fasilitas': form.get('saran_fasilitas', ''),
+        'saran_dosen': form.get('saran_dosen', ''),
+        'saran_kurikulum': form.get('saran_kurikulum', ''),
+        'saran_administrasi': form.get('saran_administrasi', ''),
+        'kekurangan': form.get('kekurangan', ''),
     }
     response = requests.post(URL+PORT+'/kuisioner/consumer', data=json.dumps(formData), headers={
         'app-origins': "yes",
@@ -169,12 +205,11 @@ async def newConsumerKuisioner(request: Request):
 async def updateConsumerKuisioner(request: Request, id: str):
     form = await request.json()
     formData = {
-        'saran_prodi': form.get('saran_prodi'),
-        'saran_fasilitas': form.get('saran_fasilitas'),
-        'saran_dosen': form.get('saran_dosen'),
-        'saran_kurikulum': form.get('saran_kurikulum'),
-        'saran_administrasi': form.get('saran_administrasi'),
-        'kekurangan': form.get('kekurangan'),
+        'saran_fasilitas': form.get('saran_fasilitas', ''),
+        'saran_dosen': form.get('saran_dosen', ''),
+        'saran_kurikulum': form.get('saran_kurikulum', ''),
+        'saran_administrasi': form.get('saran_administrasi', ''),
+        'kekurangan': form.get('kekurangan', ''),
     }
     response = requests.put(URL+PORT+'/kuisioner/consumer/'+id, data=json.dumps(formData), headers={
         'app-origins': "yes",
